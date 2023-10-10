51e96a72b757621c959bcd3cd760c408babc7341
vineeshakodipaka <vineeshakodipaka11@gmail.com>
2023-10-10 12:51:18 +0530

    Initial commit-1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-// import logo from './logo.svg';
-import React, { useState, useEffect } from "react";
-import { Col, Row } from "react-bootstrap";
-import "./App.css";
-import Content from "./components/Registerpage/Content";
-import Register from "./components/Registerpage/Register";
+import React from "react";
 
-function App() {
-  // Effect hook to add custom class to the body
-  useEffect(() => {
-    document.body.classList.add("donarcustom-body");
-    return () => {
-      document.body.classList.remove("donarcustom-body");
-    };
-  }, []);
-  return (
-    <div className="App">
-      <Row className="flex-column-reverse flex-lg-row">
-        <Col lg={7} className="order-lg-1">
-          <Content />
-        </Col>
-        <Col lg={5} className="order-lg-2">
-          <Register />
-        </Col>
-      </Row>
-    </div>
-  );
-}
+const App = () => {
+  return <div>Hello</div>;
+};
 
 export default App;
